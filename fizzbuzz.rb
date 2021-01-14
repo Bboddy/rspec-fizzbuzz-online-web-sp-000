e1dbc8c4be2468bdaef144396e97063686f2eda2
Bryce B <bryce.boddy@gmail.com>
2021-01-14 13:12:50 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
-
+  string = nil
+  string + "Fizz" if (num % 3 == 0)
+  string + "Buzz" if (num % 5 == 0)
+  return string ? (!string.nil?) : return num
 end
