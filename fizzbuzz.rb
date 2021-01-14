72fd8717f5077c40b16c6b8925d688e7a6d7ccf5
Bryce B <bryce.boddy@gmail.com>
2021-01-14 13:19:35 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,7 +2,7 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(num)
   string = nil
-  string += "Fizz" if (num % 3 == 0)
-  string += "Buzz" if (num % 5 == 0)
+  string << "Fizz" if (num % 3 == 0)
+  string << "Buzz" if (num % 5 == 0)
   return string
 end
